674b046c0c96eb86053f740b7b2fc25539010eba
Tom Maurer <tom.c.maurer@gmail.com>
2019-10-02 17:36:56 -0600

    added square

---
+++ Lab4/app/src/main/java/com/example/lab4/Square.java
@@ -0,0 +1,11 @@
+package com.example.lab4;
+
+public class Square extends Shape {
+
+    public int sideLength;
+
+    public Square(int sideLength) {
+        super();
+        this.sideLength = sideLength;
+    }
+}
